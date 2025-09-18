5b893a3525ab9542b76026bdfcd00cb09279326c
Suyndara <souzamatheussilva41@gmail.com>
2025-09-17 21:27:42 -0300

    feat: Adicionando o Thymeleaf e o Bean validation na aplicação Spring Boot

--- src/main/java/br/com/carstore/controller/HomeController.java
+++ src/main/java/br/com/carstore/controller/HomeController.java
@@ -1,9 +1,7 @@
 package br.com.carstore.controller;
 
-import br.com.carstore.model.Car;
 import br.com.carstore.model.CarDTO;
 import br.com.carstore.service.CarService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,53 +13,37 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private CarService carService;
+    private final CarService carService;
 
     public HomeController(CarService carService) {
         this.carService = carService;
     }
 
+    // Página inicial
     @GetMapping("/")
     public String index(Model model) {
-
         model.addAttribute("carDTO", new CarDTO());
-
         return "index";
-
     }
 
-    @GetMapping("/index")
-    public String indexTwo(Model model) {
-
-        model.addAttribute("carDTO", new CarDTO());
-
-        return "index";
-
-    }
-
+    // Criar novo carro
     @PostMapping("/cars")
     public String createCar(@ModelAttribute CarDTO car, Model model) {
-
         carService.save(car);
 
         List<CarDTO> cars = carService.findAll();
-
         model.addAttribute("cars", cars);
 
         return "dashboard";
-
     }
 
+    // Listar todos os carros
     @GetMapping("/cars")
     public String getAllCars(Model model) {
-
         List<CarDTO> cars = carService.findAll();
-
         model.addAttribute("cars", cars);
 
         return "dashboard";
-
     }
-
 
 }
